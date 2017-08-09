9faa57203c13352a223d80a2cb3d1148cd9d8fc3
JohnHLove <jhlove87@gmail.com>
2017-08-09 13:14:01 -0500

    finish nav bar functionality. waiting on routes

--- src/Components/BaseLayout/NavBar/index.js
+++ src/Components/BaseLayout/NavBar/index.js
@@ -7,13 +7,15 @@ const NavBar = (props) => {
     return (
       // Home(nowplaying), upcoming, popular, top rated
       <div id="navWrapper">
-        <div id="dropDownButton"><div id="arrow"></div></div>
         <div id="dropDownLinks">
           <a href="/" className="navLink">Home</a>
           <a href="/upcoming" className="navLink">Upcoming</a>
           <a href="/popular" className="navLink">Popular</a>
           <a href="/top-rated" className="navLink">Top Rated</a>
         </div>
+        <div id="dropDownButton"><div id="arrowWrap">
+          <div id="arrow"></div>
+        </div></div>
       </div>
     );
 }
