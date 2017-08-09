f3bd7092042f07c037fba4bb53349318bd8c363b
Zach Morse <zacharymorse@gmail.com>
2017-08-09 14:55:22 -0500

    routine commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
-import '../App.css';
-import BaseLayout from './BaseLayout';
-import Details from './Details';
-import Movie from './Movie';
-
-
+import React, { Component } from "react";
+import "../App.css";
+import BaseLayout from "./BaseLayout";
+import Details from "./Details";
+import Movie from "./Movie";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <BaseLayout>
-          <Movie/>
-          <Details/>
+          <Movie />
+          <Details />
         </BaseLayout>
       </div>
     );
