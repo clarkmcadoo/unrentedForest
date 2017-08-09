41d2cdb47bb0489c9bb851c295f80d174897372e
Jordan Gregory <jordangregory@gmail.com>
2017-08-09 15:44:09 -0500

    reordered css map props to baselayout and added search bar

--- src/Components/BaseLayout/index.js
+++ src/Components/BaseLayout/index.js
@@ -3,15 +3,15 @@ import "./main.css";
 import NavBar from "./NavBar";
 import { connect } from "react-redux";
 import SearchBar from "./SearchBar";
+import { movieGetter } from "../../actions";
 
 const BaseLayout = props => {
   return (
     <div id="baseLayoutWrapper">
       <div id="headerWrapper">
-        <div id="logoWrapper">
-          <h1 id="headerLogo">U.F.</h1>
-        </div>
-        <SearchBar />
+        <h1 id="headerLogo">U.F.</h1>
+
+        <SearchBar handleSubmit={props.handleSubmit} />
       </div>
       <NavBar />
       {props.children}
