e7d45f093e580555fb48f6705b0841307a1b45ec
rickmurdock <rick.murdock@live.com>
2017-08-10 08:42:47 -0500

    Updates to Details component files.

---
+++ src/Components/Details/DetailValues.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DetailValues extends Component {
+  render() {
+    return (
+      <div className="value-wrapper">
+          <h1>Movie Details</h1>
+      </div>
+    );
+  }
+}
+
+export default DetailValues;
